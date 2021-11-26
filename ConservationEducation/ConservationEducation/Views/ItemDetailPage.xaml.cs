b17b1b97c90d193b070128c4ff2028c0cd3921ac
c005476 <c0005476@newcastle.ac.uk>
2021-11-26 13:56:43 +0000

    Add project files.

---
+++ ConservationEducation/ConservationEducation/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using ConservationEducation.ViewModels;
+
+namespace ConservationEducation.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
